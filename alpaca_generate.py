8aef74b7e3a7059e8a17c692ead1f2b1e226c22a
Etash Guha <etashthebomb@gmail.com>
2024-06-19 14:22:09 -0700

    small fixes

--- alpaca_generate.py
+++ alpaca_generate.py
@@ -1,8 +1,13 @@
 import numpy as np 
 import random
-import Groq
+from groq import Groq
 import requests
 import argparse
+import datasets
+import json
+import os
+import time
+from tqdm import tqdm
 random.seed(0)
 np.random.seed(0)
 
@@ -11,67 +16,67 @@ def run_model(args):
     model = args.model
     outputs = []
 
-        for example in tqdm(eval_set):
-            instruction = example['instruction']
-            instance = {}
-            instance['instruction'] = instruction
-            instance['generator'] = model
-            instance['dataset'] = example['dataset']
-            instance['datasplit'] = "eval"
-            messages = ['role': 'user', "content": 'instruction']
-            if model == "groq":
-                client = Groq(
-                    api_key=os.environ['GROQ_API_KEY'],
-                )
-                while True:
+    for example in tqdm(eval_set):
+        instruction = example['instruction']
+        instance = {}
+        instance['instruction'] = instruction
+        instance['generator'] = model
+        instance['dataset'] = example['dataset']
+        instance['datasplit'] = "eval"
+        messages = [{'role': 'user', "content": 'instruction'}]
+        if model == "groq":
+            client = Groq(
+                api_key=os.environ['GROQ_API_KEY'],
+            )
+            while True:
+                try:
+                    chat_completion = client.chat.completions.create(
+                        messages=messages,
+                        model="llama3-8b-8192",
+                        temperature=0
+                    )
+                    response_text = chat_completion.choices[0].message.content
+                except:
+                    time.sleep(5)
+                    continue
+                break
+        elif model == "samba":
+            payload = {
+                "inputs": messages,
+                "params": {
+                    "max_tokens_allowed_in_completion": {"type": "int", "value": 500},
+                    "min_token_capacity_for_completion": {"type": "int", "value": 2},
+                    "skip_special_token": {"type": "bool", "value": True},
+                    "stop_sequences": {"type": "list", "value": ["[INST]", "[INST]", "[/INST]", "[/INST]"]}
+                },
+                "expert": "llama3-8b"
+            }
+            url = os.environ.get("SAMBA_URL")
+            key = os.environ.get("SAMBA_KEY")
+
+            headers = {
+                "Authorization": f"Basic {key}",
+                "Content-Type": "application/json"
+            }
+            post_response = requests.post(f'https://{url}/api/v1/chat/completion', json=payload, headers=headers, stream=True)
+
+            response_text = ""
+            for line in post_response.iter_lines():
+                if line.startswith(b"data: "):
+                    data_str = line.decode('utf-8')[6:]
                     try:
-                        chat_completion = client.chat.completions.create(
-                            messages=messages,
-                            model="llama3-8b-8192",
-                            temperature=0
-                        )
-                        response_text = chat_completion.choices[0].message.content
-                    except:
-                        time.sleep(5)
-                        continue
-                    break
-            elif model == "samba":
-                payload = {
-                    "inputs": messages,
-                    "params": {
-                        "max_tokens_allowed_in_completion": {"type": "int", "value": 500},
-                        "min_token_capacity_for_completion": {"type": "int", "value": 2},
-                        "skip_special_token": {"type": "bool", "value": True},
-                        "stop_sequences": {"type": "list", "value": ["[INST]", "[INST]", "[/INST]", "[/INST]"]}
-                    },
-                    "expert": "llama3-8b"
-                }
-                url = os.environ.get("SAMBA_URL")
-                key = os.environ.get("SAMBA_KEY")
+                        line_json = json.loads(data_str)
+                        content = line_json.get("stream_token", "")
+                        if content:
+                            response_text += content
+                    except json.JSONDecodeError as e:
+                        pass
+    
+        instance['output'] = response_text
+        outputs.append(instance)
 
-                headers = {
-                    "Authorization": f"Basic {key}",
-                    "Content-Type": "application/json"
-                }
-                post_response = requests.post(url, json=payload, headers=headers, stream=True)
-
-                response_text = ""
-                for line in post_response.iter_lines():
-                    if line.startswith(b"data: "):
-                        data_str = line.decode('utf-8')[6:]
-                        try:
-                            line_json = json.loads(data_str)
-                            content = line_json.get("stream_token", "")
-                            if content:
-                                response_text += content
-                        except json.JSONDecodeError as e:
-                            pass
-        
-            instance['output'] = response_text
-            outputs.append(instance)
-
-            with open(args.out_json, 'w') as f:
-                json.dump(outputs, f, indent = 4)
+        with open(args.out_json, 'w') as f:
+            json.dump(outputs, f, indent = 4)
 
 def main():
     parser = argparse.ArgumentParser()
